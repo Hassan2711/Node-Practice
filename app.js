9b40a08d84eceb76756963e9b42af054867c4218
Hassan <hassanaamer30@gmail.com>
2024-08-31 11:21:23 -0700

    2nd commit

--- app.js
+++ app.js
@@ -6,52 +6,45 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 
-// Use environment variable for MongoDB connection URI
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error("MongoDB connection error:", err));
 
 
-// Define the schema
 const UserSchema = new mongoose.Schema({
     name: String,
     age: Number,
     dateOfBirth: Date
 });
 
-// Create the model
 const User = mongoose.model('User', UserSchema);
 
 app.post('/addUser', async (req, res) => {
     try {
         const { name, age, dateOfBirth } = req.body;
-        // Create a new user instance from the request body
         const newUser = new User({
             name,
             age,
             dateOfBirth});
 
-        // Save the user to the database
         const savedUser = await newUser.save();
 
-        // Send a success response
         res.status(201).json(savedUser);
     } catch (err) {
-        // Handle errors
         res.status(400).json({ message: err.message });
     }
 });
 
 const users = [
-    { name: "Alice Johnson", age: 25, dateOfBirth: "1998-03-14" },
-    { name: "Bob Smith", age: 30, dateOfBirth: "1993-07-22" },
-    { name: "Charlie Brown", age: 35, dateOfBirth: "1988-11-10" }
+    { name: "Alice", age: 25, dateOfBirth: "1998-03-14" },
+    { name: "Bob", age: 30, dateOfBirth: "1993-07-22" },
+    { name: "Jhon", age: 35, dateOfBirth: "1988-11-10" }
 ];
 
 
 const addStaticData = async () => {
     try {
-        await User.insertMany(users); // Insert the static dataset
+        await User.insertMany(users);
         console.log("Static data added to MongoDB");
     } catch (err) {
         console.error("Error adding static data:", err);
